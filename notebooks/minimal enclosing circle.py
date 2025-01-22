3ca59a8f4823d6230f31aff389491616582237e1
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-01-22 21:55:53 +0400

    Update minimal enclosing circle.py (#58)
    * Update minimal enclosing circle.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- notebooks/minimal enclosing circle.py
+++ notebooks/minimal enclosing circle.py
@@ -1,19 +1,19 @@
 import marimo
 
-__generated_with = "0.10.9"
+__generated_with = "0.10.15"
 app = marimo.App(width="medium")
 
 
 @app.cell
 def _(mo):
-    mo.md("# Problem")
+    mo.md("""# Problem""")
     return
 
 
 @app.cell
 def _(mo):
     mo.md(
-        "We compute the radius and center of the smallest enclosing ball for $N$ points in $d$ dimensions. We use a variety of tools and compare their performance."
+        """We compute the radius and center of the smallest enclosing ball for $N$ points in $d$ dimensions. We use a variety of tools and compare their performance."""
     )
     return
 
@@ -27,7 +27,7 @@ def _():
 
 @app.cell
 def _(mo):
-    mo.md("## Generate a cloud of points")
+    mo.md("""## Generate a cloud of points""")
     return
 
 
@@ -73,7 +73,7 @@ def _(go, pos):
 
 @app.cell
 def _(mo):
-    mo.md("## Compute with cvxpy")
+    mo.md("""## Compute with cvxpy""")
     return
 
 
@@ -81,16 +81,14 @@ def _(mo):
 def _(np):
     import cvxpy as cp
 
-    # We compare 3 equivalent ways to create the constraint that
-    # each point has to be within the ball of radius r centered at x
-    def con_1(points, x, r):
-        return [cp.norm(point - x) <= r for point in points]
+    def min_circle_cvx(points, **kwargs):
+        # cvxpy variable for the radius
+        r = cp.Variable(1, name="Radius")
+        # cvxpy variable for the midpoint
+        x = cp.Variable(points.shape[1], name="Midpoint")
 
-    def con_2(points, x, r):
-        return [cp.SOC(r, point - x) for point in points]
-
-    def con_3(points, x, r):
-        return [
+        objective = cp.Minimize(r)
+        constraints = [
             cp.SOC(
                 r * np.ones(points.shape[0]),
                 points - cp.outer(np.ones(points.shape[0]), x),
@@ -98,47 +96,17 @@ def _(np):
             )
         ]
 
-    def min_circle_cvx(points, fct=None, **kwargs):
-        # Use con_1 if no constraint construction is defined
-        fct = fct or con_1
-        # cvxpy variable for the radius
-        r = cp.Variable(1, name="Radius")
-        # cvxpy variable for the midpoint
-        x = cp.Variable(points.shape[1], name="Midpoint")
-
-        objective = cp.Minimize(r)
-        constraints = fct(points, x, r)
-
         problem = cp.Problem(objective=objective, constraints=constraints)
         problem.solve(**kwargs)
 
         return {"Radius": r.value, "Midpoint": x.value}
 
-    return con_1, con_2, con_3, cp, min_circle_cvx
+    return cp, min_circle_cvx
 
 
 @app.cell
-def _(con_1, min_circle_cvx, pos):
-    min_circle_cvx(points=pos, fct=con_1, solver="CLARABEL")
-    return
-
-
-@app.cell
-def _(con_2, min_circle_cvx, pos):
-    min_circle_cvx(points=pos, fct=con_2, solver="CLARABEL")
-    return
-
-
-@app.cell
-def _(con_3, min_circle_cvx, pos):
-    min_circle_cvx(points=pos, fct=con_3, solver="CLARABEL")
-    return
-
-
-@app.cell
-def _(con_3, min_circle_cvx, pos):
-    # CLARABEL is the only option :-)
-    min_circle_cvx(points=pos, fct=con_3)
+def _(min_circle_cvx, pos):
+    min_circle_cvx(points=pos, solver="CLARABEL")
     return
 
 
