cea8cdb181f1f98cda1115fda494d9832015b0e4
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-06-11 13:02:27 +0400

    Update startup.sh

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 curl -LsSf https://astral.sh/uv/install.sh | sh
-uv venv
-uv pip install --no-cache-dir marimo
-uv pip install --no-cache-dir -r requirements.txt
+uvx marimo run --sandbox notebooks/minimal_enclosing_circle.py
